1c59ca13db80bae4503d08647d5a095a88d99917
André Suriane <48450639+andresuriane@users.noreply.github.com>
2022-10-14 01:34:48 -0300

    parte 1

---
+++ figure4_3/deseasonal.R
@@ -0,0 +1,30 @@
+# DESEASONAL.M
+# Lutz Kilian
+# University of Michigan
+# August 2010
+
+# This program fits monthly seasonal dummies to a univariate time series 
+# and returns the residuals
+
+deseasonal<-function(y) {
+
+t=length(y);
+
+X<-diag(12);
+
+for (i in 1:((t/12)+1)){
+    X=rbind(X,diag(12));
+}
+X=X[1:t,];
+
+# Regression coefficients and residuals
+bhat= solve(crossprod(X)) %*% crossprod(X,y);
+yhat=X%*%bhat;
+ehat=y-yhat;           
+    return(ehat)
+}
+
+
+
+
+
